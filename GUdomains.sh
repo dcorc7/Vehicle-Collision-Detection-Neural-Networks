773e1e5265e28948bdd695b3415cfe6d1380426d
dcorc7 <dc1524@georgetown.edu>
2025-05-02 14:50:36 -0400

    Sent to gu domains

--- GUdomains.sh
+++ GUdomains.sh
@@ -1,7 +1,7 @@
 
 
 echo "Cleaning up existing build..."
-rm -rf Vehicle Collision Detection
+rm -rf Vehicle_Collision_Detection_Project
 
 # build website
 echo "Building the website..."
@@ -10,16 +10,16 @@ quarto render
 # Set Correct File Permissions
 echo "Setting correct file permissions..."
 
-find Vehicle Collision Detection -type f -exec chmod 644 {} \;
+find Vehicle_Collision_Detection_Project -type f -exec chmod 644 {} \;
 
-find Vehicle Collision Detection -type d -exec chmod 755 {} \;
+find Vehicle_Collision_Detection_Project -type d -exec chmod 755 {} \;
 
 # push to the Website
 read -p "Do you want to push the website to your GU domains folder? (y/n): " answer
 
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "Pushing the website to the remote server..."
-    scp -r Vehicle Collision Detection Neural Networks corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    scp -r Vehicle_Collision_Detection_Project Neural Networks corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
     echo "Website successfully pushed!"
 else
     echo "Skipping deployment."
