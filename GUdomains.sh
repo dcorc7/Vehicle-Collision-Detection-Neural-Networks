c44598fe9e04999f2cc84e861afbb5c92283769e
dcorc7 <dc1524@georgetown.edu>
2025-05-02 14:47:47 -0400

    Added project content to qmd files

--- GUdomains.sh
+++ GUdomains.sh
@@ -1,7 +1,7 @@
 
 
 echo "Cleaning up existing build..."
-rm -rf Collision Detection Neural Networks
+rm -rf Vehicle Collision Detection
 
 # build website
 echo "Building the website..."
@@ -10,16 +10,16 @@ quarto render
 # Set Correct File Permissions
 echo "Setting correct file permissions..."
 
-find Collision Detection Neural Networks -type f -exec chmod 644 {} \;
+find Vehicle Collision Detection -type f -exec chmod 644 {} \;
 
-find Collision Detection Neural Networks -type d -exec chmod 755 {} \;
+find Vehicle Collision Detection -type d -exec chmod 755 {} \;
 
 # push to the Website
 read -p "Do you want to push the website to your GU domains folder? (y/n): " answer
 
 if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
     echo "Pushing the website to the remote server..."
-    scp -r Collision Detection Neural Networks corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
+    scp -r Vehicle Collision Detection Neural Networks corcoran@corcoran.georgetown.domains:/home/corcoran/public_html/
     echo "Website successfully pushed!"
 else
     echo "Skipping deployment."
